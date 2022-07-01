5d8e6cfc9ff4e20bb98f0a94fe5a559f56b67610
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-01 16:13:19 +0700

    hello name

---
+++ Hello/src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name");
+        String name= scanner.next();
+        System.out.printf("Hello %s",name);
+
+    }
+}
