418a21e43fed57110b9f211c5c281fa400c843f1
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-04 09:21:38 +0700

    find greatest common divisor

---
+++ lesson2-loop-in-java/find-greatest-common-divisor/src/GreatestCommonDivisor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number 1:");
+        int number1= scanner.nextInt();
+        System.out.println("Enter number 2:");
+        int number2= scanner.nextInt();
+        number1= Math.abs(number1);
+        number2= Math.abs(number2);
+        if(number1==0 || number2 == 0){
+            System.out.println("No greatest common factor");
+        } else {
+            while (number1!=number2){
+                if(number1>number2)
+                    number1= number1- number2;
+                else number2 = number2- number1;
+            }
+            System.out.println("Greatest common factor: "+number1);
+        }
+
+    }
+}
