a67bcbd263ccc62e9c0f3c8e248af81196b44a4b
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-08 11:00:22 +0700

    access modifier

---
+++ lesson5-access-modifier-static-method-static-property/access-modifier/src/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle cir1= new Circle(2);
+        System.out.println(cir1.getRadius());
+        System.out.println(cir1.getArea());
+    }
+}
