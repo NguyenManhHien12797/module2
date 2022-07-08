9cc8f5608d2557e11a59c84c36ea15ce418a97b7
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-08 11:32:37 +0700

    write only class contruction

---
+++ lesson5-access-modifier-static-method-static-property/write-only-class-contruction/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Student a= new Student();
+        System.out.println(a.getName());
+        a.setName("Nam");
+        System.out.println(a.getName());
+    }
+
+}
