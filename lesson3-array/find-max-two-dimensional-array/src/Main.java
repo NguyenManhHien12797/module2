8a6c50969fd7277acd90f5a30fe47dfee95951eb
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-11 11:56:40 +0700

    find max two dimensional array

--- lesson3-array/find-max-two-dimensional-array/src/Main.java
+++ lesson3-array/find-max-two-dimensional-array/src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = new int[10][10];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = (int) (Math.random() * 100);
+            }
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                System.out.print(matrix[row][column] + " ");
+            }
+            System.out.println();
+        }
+        int max= matrix[0][0];
+        int indexi=0;
+        int indexj=0;
+        for(int i = 0; i<matrix.length; i++){
+            for(int j=0; j< matrix[i].length;j++){
+                if(matrix[i][j]>max){
+                    max= matrix[i][j];
+                    indexi= i;
+                    indexj= j;
+                }
+            }
+        }
+        System.out.println("index: "+indexi+"-"+ indexj);
+        System.out.println("Max là: "+max);
+
+    }
 }
